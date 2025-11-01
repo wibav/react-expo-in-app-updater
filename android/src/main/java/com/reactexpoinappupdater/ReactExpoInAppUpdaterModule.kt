5ae369ac120ca2bedb5877ac232de38b6b259d62
Cristian Da Silva <cdasilva@nexo-it.cl>
2025-11-01 17:37:20 +0000

    fix: use reactApplicationContext.currentActivity for better compatibility with React Native versions

--- android/src/main/java/com/reactexpoinappupdater/ReactExpoInAppUpdaterModule.kt
+++ android/src/main/java/com/reactexpoinappupdater/ReactExpoInAppUpdaterModule.kt
@@ -27,7 +27,8 @@ class ReactExpoInAppUpdaterModule(reactContext: ReactApplicationContext) :
 
     @ReactMethod
     fun checkForUpdate(updateType: String, promise: Promise) {
-        val activity = currentActivity ?: return promise.reject("NO_ACTIVITY", "No current activity")
+        val activity = reactApplicationContext.currentActivity 
+            ?: return promise.reject("NO_ACTIVITY", "No current activity")
 
         appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
